577ed7d83dd696e3096120027640036d2f287236
yehuda hason <yehudaxt@gmail.com>
2025-05-07 20:44:34 +0300

    final1

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-const PORT = 2500;
-app.use(express.static(__dirname));
-
-app.listen(PORT ,console.log(`Server running at http://localhost:${PORT}/`));
